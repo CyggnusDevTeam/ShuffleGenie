6cc5bdc5d1154a099346ed3a557e244155efba14
lkotlarenko <lkotlarenko@proton.me>
2023-02-23 15:34:15 -0300

    fix: 🐛 number of collection cards

--- src/Utils/dataScraper.js
+++ src/Utils/dataScraper.js
@@ -2,18 +2,26 @@ import { DEFAULT_USER } from './variables';
 
 const { REACT_APP_API_URL } = process.env;
 
-const makeRequest = async (user, setCollection, setLCT, setIsLoading) => {
+const makeRequest = async (
+  user,
+  setCollection,
+  setLCT,
+  setIsLoading,
+  setCardsNum
+) => {
   setLCT(Date.now());
   const API_URL = `${REACT_APP_API_URL}${user}`;
   if (API_URL === REACT_APP_API_URL) {
     const response = await fetch(`${API_URL}${DEFAULT_USER}`);
     const data = await response.json();
     setCollection(data);
+    setCardsNum(data.length + 1);
     setIsLoading(false);
   } else {
     const response = await fetch(API_URL);
     const data = await response.json();
     setCollection(data);
+    setCardsNum(data.length + 1);
     setIsLoading(false);
   }
 };
@@ -23,14 +31,15 @@ const fetchCollection = async (
   setCollection,
   lastCalledTime,
   setLCT,
-  setIsLoading
+  setIsLoading,
+  setCardsNum
 ) => {
   if (lastCalledTime === 'first') {
-    await makeRequest(user, setCollection, setLCT, setIsLoading);
+    await makeRequest(user, setCollection, setLCT, setIsLoading, setCardsNum);
   }
   // Do not call the API too many times
   else if (Date.now() - lastCalledTime >= 4000) {
-    await makeRequest(user, setCollection, setLCT, setIsLoading);
+    await makeRequest(user, setCollection, setLCT, setIsLoading, setCardsNum);
   }
 };
 
