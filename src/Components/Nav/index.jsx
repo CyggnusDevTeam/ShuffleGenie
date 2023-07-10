f26fafecc5658ea99faa00da2a78a599bad02d02
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-07-10 01:23:27 -0300

    feat: ✨ new navBar functions
    new:
     add 'Sync' button to 'Nav'
     changed:
     updated Links from 'Nav'.
     updated Spinner color to better match app colors.

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,27 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AppContext from '../../Context/AppContext';
+import populateCollectionData from '../../Utils/populateCollectionData';
 
 function Nav() {
+  const {
+    lastCalledTime,
+    needSync,
+    setCardsNum,
+    setCollection,
+    setIsLoading,
+    setLCT,
+    username,
+  } = useContext(AppContext);
+  const [APIError, setAPIError] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const handleClick = () => {
+    setIsButtonDisabled(true);
+    try {
+      populateCollectionData(
+        username,
+        setCollection,
+        setCardsNum,
+        setIsLoading,
+        lastCalledTime
+      );
+      setAPIError(false);
+    } catch (error) {
+      setAPIError(true);
+    }
+    setLCT(Date.now());
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 30000);
+  };
+
   return (
-    <nav className="bg-gray-3">
+    <nav className="bg-gray-3 mb-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20">
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center">
-            <h1 className="text-white text-3xl font-bold">ShuffleGenie</h1>
+            <Link to="/" className="navLink">
+              <h1 className="text-white text-3xl font-bold">ShuffleGenie</h1>
+            </Link>
           </div>
           <div className="flex items-center">
             <ul className="flex space-x-4">
-              <li>
-                <Link to="/" className="navLink">
-                  Home
-                </Link>
-              </li>
-
-              <li>
-                <Link to="/" className="navLink">
-                  lorem input
-                </Link>
-              </li>
+              {!needSync && (
+                <li>
+                  <Link to="/collection" className="navLink">
+                    Collection
+                  </Link>
+                </li>
+              )}
 
               <li>
                 <Link to="/help" className="navLink">
@@ -31,9 +63,16 @@ function Nav() {
             </ul>
           </div>
           <div>
-            <button type="button" className="defaultButton">
-              Profile
-            </button>
+            {!needSync && (
+              <button
+                type="button"
+                onClick={handleClick}
+                disabled={isButtonDisabled}
+                className="defaultButton">
+                Sync
+              </button>
+            )}
+            {APIError && <p className="text-red">Failed to contact API!</p>}
           </div>
         </div>
       </div>
