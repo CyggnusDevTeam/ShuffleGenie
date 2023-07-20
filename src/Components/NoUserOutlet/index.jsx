b0e209cc7fe8122cccfe9c98ba3f844ba193f969
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-07-20 19:53:56 -0300

    refactor: ♻️ format DeckBuilder and NoUserOutlet

--- src/Components/NoUserOutlet/index.jsx
+++ src/Components/NoUserOutlet/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import SyncPage from '../../Pages/SyncPage';
+import SyncForm from '../SyncForm';
 import cardImg from '../../Img/card.png';
 
 function NoUserOutlet() {
@@ -10,34 +10,34 @@ function NoUserOutlet() {
 
   return (
     <>
-      <section className='flex justify-around bg-gray-1 h-screen'>
-        <div className='flex flex-col justify-center items-center max-w-screen w-4/12 space-y-16'>
-          <h2 className='text-violet-1 h2Title'>
+      <section className="flex justify-around bg-gray-1 h-screen">
+        <div className="flex flex-col justify-center items-center max-w-screen w-4/12 space-y-16">
+          <h2 className="text-violet-1 h2Title">
             Welcome to the ShuffleGenie 👋
           </h2>
-          <h1 className='h1Title'>Create a new random deck with your cards!</h1>
-          <p className='defaultPageText'>
+          <h1 className="h1Title">
+            Create a new random deck with your owned collection cards!
+          </h1>
+          <p className="defaultPageText">
             Sync your Marvel Snap Zone account username to generate a new
             completely random deck with cards from your snap collection.
           </p>
           <button
-            type='button'
+            type="button"
             onClick={focusSyncSection}
-            className='defaultButton'
-          >
+            className="defaultButton">
             SYNC YOUR COLLECTION
           </button>
         </div>
-        <div className='flex flex-col justify-center'>
-          <img className='flex max-w-xs max-h-96' src={cardImg} alt='card' />
+        <div className="flex flex-col justify-center">
+          <img className="flex max-w-xs max-h-96" src={cardImg} alt="card" />
         </div>
       </section>
       <section
         ref={syncSectionRef}
-        id='sync-section'
-        className='bg-gray-2 h-96 flex flex-col justify-center'
-      >
-        <SyncPage />
+        id="sync-section"
+        className="bg-gray-2 h-96 flex flex-col justify-center">
+        <SyncForm />
       </section>
     </>
   );
