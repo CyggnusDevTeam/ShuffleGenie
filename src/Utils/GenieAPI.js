f4640af9c1423174f6f08c7f8b06cc7be2900d5e
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-07-21 15:41:28 -0300

    fix: 🐛 add try catch to genieAPI call
    add:
     new error variable.

--- src/Utils/GenieAPI.js
+++ src/Utils/GenieAPI.js
@@ -1,12 +1,16 @@
-import { ERROR_TOO_MANY } from './variables';
+import { ERROR_FAILED_COLLECTION, ERROR_TOO_MANY } from './variables';
 
 const { REACT_APP_API_URL } = process.env;
 
 const makeAPIRequest = async (user) => {
-  const API_URL = `${REACT_APP_API_URL}${user}`;
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  return data;
+  try {
+    const API_URL = `${REACT_APP_API_URL}?user=${user}`;
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw Error(ERROR_FAILED_COLLECTION);
+  }
 };
 
 const fetchCollection = async (user, lastCalledTime) => {
--- src/Utils/variables.js
+++ src/Utils/variables.js
@@ -1,5 +1,11 @@
 const DEFAULT_USER = 'DefaultPool2';
+const ERROR_FAILED_COLLECTION = 'ERROR: Failed to fetch collection';
 const ERROR_TOO_MANY = 'ERROR: Too many API calls!';
 const BACKEND_URL = 'http://localhost:3001/user/';
 
-export { DEFAULT_USER, BACKEND_URL, ERROR_TOO_MANY };
+export {
+  DEFAULT_USER,
+  BACKEND_URL,
+  ERROR_FAILED_COLLECTION,
+  ERROR_TOO_MANY,
+};
