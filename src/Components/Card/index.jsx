39e6bb80f4fffc6db699fa80273f6339293ff818
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-07-21 18:19:49 -0300

    feat: 🚸 add loading skeleton for card Images

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import lazyCard from '../../Img/lazyCard.png';
 
 function Card({ cardName, cardImg }) {
+  const convertToSlug = (inputString) =>
+    inputString.toLowerCase().replace(/\s+/g, '-');
+
+  // State to track whether the actual image is loaded
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  // Handler function for when the actual image is loaded
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div className='inline-block relative group mb-14'>
       <h2 className='text-white text-center font-bold text-lg'>{cardName}</h2>
-      <img
-        className='max-w-[300px] max-h-[300px] cursor-pointer transition-transform duration-300 transform-gpu group-hover:-translate-y-1 group-hover:scale-105'
-        src={cardImg}
-        alt={cardName}
-      />
+      <a
+        href={`https://marvelsnapzone.com/cards/${convertToSlug(cardName)}`}
+        target='_blank'
+        rel='noopener noreferrer'
+        title={`View ${cardName} on MarvelSnapZone©`}>
+        {!imageLoaded && (
+          <img
+            className='max-w-[300px] max-h-[300px] cursor-pointer transition-transform duration-300 transform-gpu group-hover:-translate-y-1 group-hover:scale-105'
+            src={lazyCard}
+            alt={cardName}
+          />
+        )}
+        <img
+          className={`max-w-[300px] max-h-[300px] cursor-pointer transition-transform duration-300 transform-gpu group-hover:-translate-y-1 group-hover:scale-105 ${
+            !imageLoaded ? 'hidden' : ''
+          }`}
+          src={cardImg}
+          alt={cardName}
+          onLoad={handleImageLoad}
+        />
+      </a>
     </div>
   );
 }
