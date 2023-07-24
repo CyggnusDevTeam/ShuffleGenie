0c67f2098b8345c271a0cb4141e4547363fcd935
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-07-24 19:39:16 -0300

    feat: 💄 update profile page welcoming text
    changed:
     update profile page to show another text when using Default user.

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -5,6 +5,7 @@ import Spinner from '../../Components/Spinner';
 
 function Profile() {
   const { cardsNum, collection, isLoading, username } = useContext(AppContext);
+  const isDefaultUser = username === 'DefaultPool2';
 
   return (
     <section className='bg-gray-1'>
@@ -14,7 +15,11 @@ function Profile() {
         <>
           {collection && (
             <div className='my-[5%] flex items-start justify-center'>
-              <h2 className='defaultPageText'>{`${username}, thats your collection, you have ${cardsNum} cards!`}</h2>
+              {isDefaultUser ? (
+                <h2 className='defaultPageText'>{`Welcome to our app👽, you are exploring the Default Collection with ${cardsNum} cards! Enjoy the diverse selection of cards from Pool 2 and below.`}</h2>
+              ) : (
+                <h2 className='defaultPageText'>{`Hi ${username}👽, here is your snap collection of ${cardsNum} cards!`}</h2>
+              )}
             </div>
           )}
           <div className='flex flex-wrap justify-center mx-auto px-20'>
