14427538df3f40a80ec218e48d4e3b42800b34fe
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-08-02 01:06:58 -0300

    feat: 🌐 add localization files to 'src/i18n/locales'

--- src/Pages/NewUser/index.tsx
+++ src/Pages/NewUser/index.tsx
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import SyncForm from '../../Components/SyncForm';
 import cardImg from '../../Img/cardHome.webp';
 
 const NewUser: React.FC = () => {
   const syncSectionRef = useRef<HTMLDivElement>(null);
+  const { t } = useTranslation();
   const focusSyncSection = () => {
     syncSectionRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
@@ -13,20 +15,19 @@ const NewUser: React.FC = () => {
       <section className='z-20 flex flex-col justify-center items-center mt-[10%] sm:mt-[15%] lg:m-[12%] lg:flex-row lg:justify-around bg-gray-1'>
         <div className='flex flex-col justify-center items-center max-w-screen lg:w-4/12 space-y-11 sm:space-y-16'>
           <h2 className='text-violet-1 text-center lg:mt-0 font-bold text-2xl max-w-[90%] lg:max-w-[100%]'>
-            Welcome to ShuffleGenie 👋
+            {t('title')}
           </h2>
           <h1 className='h1Title max-w-[90%] lg:max-w-[100%]'>
-            Create a new random deck with your owned collection cards!
+            {t('description.part1')}
           </h1>
           <p className='defaultPageText max-w-[90%] lg:max-w-[100%]'>
-            Sync your Marvel Snap Zone account username to generate a new
-            completely random deck with cards from your snap collection.
+            {t('description.part2')}
           </p>
           <button
             type='button'
             onClick={focusSyncSection}
             className='defaultButton'>
-            SYNC YOUR COLLECTION
+            {t('syncCollection')}
           </button>
         </div>
         <div className='flex flex-col justify-center mb-[55%] sm:mb-[25%] md:mb-[25%] lg:mb-0'>
