f07bf520b35e98118add6053dea410b2e5c07869
Leonardo Kotlarenko <lkotlarenko@proton.me>
2023-08-02 15:56:12 -0300

    feat: 🌐 add locales to 'Profile' page

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import AppContext from '../../Context/AppContext';
 import Card from '../../Components/Card';
 import LoadingSpinner from '../../Components/LoadingSpinner';
 import { CollectionItem } from '../../Interfaces/CollectionItem';
 
 const Profile: React.FC = () => {
+  const { t } = useTranslation();
   const { cardsNum, collection, isLoading, needSync, username } =
     useContext(AppContext);
   const isDefaultUser = username === 'DefaultPool2';
@@ -28,17 +30,25 @@ const Profile: React.FC = () => {
           {collection && (
             <div className='my-[5%] px-3 flex flex-col items-center justify-center space-y-4'>
               {isDefaultUser ? (
-                <h2 className='defaultPageText'>{`Welcome to our app 👽, you are exploring the Default Collection with ${cardsNum} cards! Enjoy the diverse selection of cards from Pool 2 and below.`}</h2>
+                <h2 className='defaultPageText'>
+                  {t('profile.greetings.defaultUser', { numCards: cardsNum })}
+                </h2>
               ) : (
-                <h2 className='defaultPageText'>{`Hi ${username} 👽, here is your snap collection of ${cardsNum} cards!`}</h2>
+                <h2 className='defaultPageText'>
+                  {t('profile.greetings.loggedUser', {
+                    user: username,
+                    numCards: cardsNum,
+                  })}
+                </h2>
               )}
               <h3 className='defaultPageText'>
-                To generate random decks click{' '}
+                {t('profile.info.part1')}
                 <button
                   className='text-violet-1'
                   type='button'
+                  title={t('nav.btnTitle.home')}
                   onClick={handleRedirectHome}>
-                  here
+                  {t('profile.info.part2')}
                 </button>
                 .
               </h3>
